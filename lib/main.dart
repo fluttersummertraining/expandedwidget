21e1d703818dcd7943a50ff3d0b4e2292c9ab328
theMelonMusk <qmindzoom@gmail.com>
2022-06-08 12:07:19 +0530

    adding vertical flex and completing the design, using padding widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,46 +15,63 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blueGrey,
         title: Text("Responsive UI"),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              SizedBox(
-                width: 10,
-              ),
-              Expanded(
-                flex: 2,
-                child: customRoundedContainer(
-                  width: 250,
-                  height: 200,
-                ),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Expanded(
-                flex: 1,
-                child: customRoundedContainer(
-                  width: 150,
-                  height: 200,
-                ),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              //customRoundedContainer(),
-            ],
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(flex: 2, child: designerRowCreation()),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(flex: 1, child: customRoundedContainer()),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(flex: 2, child: designerRowCreation()),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(flex: 1, child: customRoundedContainer()),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-Widget customRoundedContainer({required double width, required double height}) {
+Widget designerRowCreation() {
+  return Row(
+    children: [
+      Expanded(
+        flex: 2,
+        child: customRoundedContainer(
+            //width: 250,
+            //height: 200,
+            ),
+      ),
+      SizedBox(
+        width: 10,
+      ),
+      Expanded(
+        flex: 1,
+        child: customRoundedContainer(
+            //width: 150,
+            //height: 200,
+            ),
+      ),
+      //customRoundedContainer(),
+    ],
+  );
+}
+
+Widget customRoundedContainer() {
   return Container(
-    width: width,
-    height: height,
     decoration: BoxDecoration(
       color: Colors.blueGrey,
       borderRadius: BorderRadius.circular(
